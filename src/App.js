906ba22859bee33963b379f3eb1016d5c870104e
devgirlpro <agalidari@gmail.com>
2024-04-21 16:53:55 +0200

    improve search finctionality to start searching only after clicking Enter

--- src/App.js
+++ src/App.js
@@ -4,10 +4,16 @@ import { Users } from './users';
 
 function App() {
   const [query, setQuery] = useState('');
+  const [filteredUser, setFilteredUser] = useState([]);
 
-  console.log(
-    Users.filter((user) => user.first_name.toLowerCase().includes(query))
-  );
+const searchHandle = (event) => {
+  if(event.key === 'Enter') {
+    const filtered = Users.filter((user) => user.first_name.toLocaleLowerCase().includes(query.toLocaleLowerCase()))
+    setFilteredUser(filtered)
+  } else {
+    setQuery(event.target.value)
+  }
+}
   return (
     <div className="app">
       <input
@@ -15,17 +21,15 @@ function App() {
         placeholder="Search..."
         className="search"
         onChange={(e) => setQuery(e.target.value)}
+        value={query}
+        onKeyDown={searchHandle}
       />
       <ul className="list">
-        {Users.filter((user) =>
-          user.first_name.toLocaleLowerCase().includes(query)
-        ).map((user) => {
-          return (
-            <li key={user.id} className="listItem">
-              {user.first_name}
-            </li>
-          );
-        })}
+       {filteredUser.length > 0 ? (
+        filteredUser.map((user) => (
+          <li key={user.id} className="listItem">{user.first_name}</li>
+        ) )
+       ) : (<li><h3>No Resoult Found !</h3></li>)}
       </ul>
     </div>
   );
